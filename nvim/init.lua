d966c952ff4f0714dfcdf163d21dc53d02ea1119
Jan Steikowski <jan.steikowski@buzzwoo.de>
2024-03-19 12:49:21 +0700

    Init commit - setting up neovim

---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+require('user/plugins')
+require('user/options')
+require('user/keymaps')
+
+vim.g.mkdp_echo_preview_url = 1
+vim.g.mkdp_auto_start = 1
+vim.g.mkdp_port = 8894
---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,42 @@
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wildmode = 'longest:full,full' -- complete the longest common match,, and allow tabbing the results to fully complete them
+
+vim.opt.title = true
+vim.opt.mouse = 'a' -- enable mouse for all modes
+
+vim.opt.termguicolors = true
+vim.opt.spell = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.list = true -- enable the below listchars
+vim.opt.listchars = { tab = '◀ ', trail = '.' }
+vim.opt.fillchars:append({ eob = ' ' }) -- remove the ~ from end of buffer
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.scrolloff = 5 
+vim.opt.sidescrolloff = 5
+
+vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
+
+vim.opt.signcolumn = 'yes:1'
+
+vim.opt.confirm = true -- ask for confirmation instead of erroring
+
+vim.opt.undofile = true -- persistent undo
+vim.opt.backup = true -- automatically save a backup file
+vim.opt.backupdir:remove('.') -- keep backup out of the current directory
