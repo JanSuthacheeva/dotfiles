f6a9fd0113bd2bcc1f9e3a636608d03f9f6c6934
Jan Suthacheeva <jan.steikowski@buzzwoo.de>
2024-07-21 09:57:52 +0700

    Update nvim settings

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,8 +1,12 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 vim.keymap.set('n', '<Leader>n', ':NvimTreeFindFileToggle<CR>')
 -- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
+vim.keymap.set('n', '<Leader>x', ':bdelete<CR>')
 -- Reselect visual selection after indenting.
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
--- nvim/lua/user/plugins/plugins.lua
+++ nvim/lua/user/plugins/plugins.lua
@@ -17,7 +17,38 @@ return {
     "nvim-neorg/neorg",
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
-    config = true,
+    config = function()
+      require("neorg").setup({
+        -- Tell Neorg what modules to load
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {
+            config = {
+              icon_preset = "diamond",
+              icons = {
+                code_block = {
+                  conceal = true,
+                }
+              }
+            }
+          },
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "$HOME/projects/notes/neorg"
+              }
+            }
+          },
+          ["core.export"] = {},
+          ["core.keybinds"] = {
+            config = {
+              default_keybinds = true,
+              neorg_leader = "<Leader>o"
+            }
+          }
+        }
+      })
+    end
   }),
   -- Packer can manage itself.
   ('wbthomason/packer.nvim'),
