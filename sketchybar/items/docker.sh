e18f41e9517daabe92892a75b354a5e40c818235
Jan Suthacheeva <j.steikowski@aol.com>
2025-08-08 21:33:53 +0700

    Add docker tooltip

--- sketchybar/items/docker.sh
+++ sketchybar/items/docker.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+POPUP_CLICK_SCRIPT="sketchybar --set \$NAME popup.drawing=toggle"
+
 docker=(
   script="$PLUGIN_DIR/docker.sh"
   icon.font="$FONT:Regular:19.0"
   padding_right=5
   padding_left=0
-  update_freq=30
+  update_freq=20
+  popup.align=right
+  click_script="$POPUP_CLICK_SCRIPT"
 )
 
 sketchybar --add item docker right      \
